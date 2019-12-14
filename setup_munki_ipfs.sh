4a740fe7ab53b12d0957e800cfe1b323806ac5a5
Nate Walck <nate.walck@gmail.com>
2019-12-14 16:19:48 -0500

    Updated setup script to install ipfs middleware

--- setup_munki_ipfs.sh
+++ setup_munki_ipfs.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+echo "Validing requirements..."
 # Make sure requirements are installed before running setup
 command -v autopkg >/dev/null 2>&1 || { echo >&2 "Install autopkg before continuing"; exit 1; }
 command -v ipfs >/dev/null 2>&1 || { echo >&2 "Install ipfs before continuing"; exit 1; }
@@ -13,16 +14,24 @@ command -v makecatalogs >/dev/null 2>&1 || { echo >&2 "Install munki admin tools
 
 # Configure munki repo setting for autopkg
 MUNKI_REPO="/Users/Shared/munki_repo"
-defaults write com.github.autopkg MUNKI_REPO "$MUNKI_REPO"
-defaults write ~/Library/Preferences/com.googlecode.munki.munkiimport repo_url "file://$MUNKI_REPO"
+/usr/bin/defaults write com.github.autopkg MUNKI_REPO "$MUNKI_REPO"
+/usr/bin/defaults write ~/Library/Preferences/com.googlecode.munki.munkiimport repo_url "file://$MUNKI_REPO"
 
 # Place autopkg runner script
+echo "Placing autopkg runner script..."
 /bin/cp ./autopkg/autopkg_runner.sh /Users/Shared/autopkg/
 /bin/chmod +x /Users/Shared/autopkg/autopkg_runner.sh
 /bin/cp ./autopkg/app_list.txt /Users/Shared/autopkg/
 
-# Move recipe overrides into place
+# Copy recipe overrides into place
+echo "Installing autopkg recipe overrides..."
 /bin/cp -r ./autopkg/RecipeOverrides/* ~/Library/AutoPkg/RecipeOverrides/
+
+# Copy ipfs middleware into place 
+echo "Installing IPFS middleware..."
+/usr/bin/sudo /bin/cp -r ./middleware/middleware_ipfs.py /usr/local/munki/
+/usr/bin/sudo /usr/sbin/chown root:wheel /usr/local/munki/middleware_ipfs.py
+/usr/bin/sudo /bin/chmod 600 /usr/local/munki/middleware_ipfs.py
 
 # Add a few autopkg recipe repos
 echo "Adding natewalck recipes..."
